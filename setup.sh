cfb33e6dd6383ce98cb00cf87901d6bbcf57b78a
John-YOU <yhlfc@hotmail.com>
2017-09-13 17:46:07 +0800

    fix script

--- setup.sh
+++
@@ -1,8 +0,0 @@
-wget https://github.com/John-YOU/System-Monitoring-tools/archive/master.zip
-unzip master.zip
-cd System-Monitoring-tools-master/nethogs
-sudo apt-get install default-jdk
-make clean
-sudo make install
-cd ../work-place
-chmod a+x ./work.sh
